6ce9b1480df792aa692ca9e39939cd08025c9eb4
maymike321 <yamekim@comcast.net>
2019-08-11 03:11:13 -0600

    Update version to 1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='pyykov',
-    version='0.1',
+    version='1.0',
     description='Markov generator for python.',
     url='https://github.com/maymike321/pyykov',
     author='Michael May',
